ab06c90625840d11078c03adf9bcc91cd0adec50
aureliepluche1 <aurelie.pluche@mail.mcgill.ca>
2016-03-29 09:49:39 -0400

    Create test.java

---
+++ test.java
@@ -0,0 +1,15 @@
+
+public class test
+{
+  
+  public void testFlatRateDiscountStrategy()
+  {
+    int amt = 100;
+    FlatRateDiscountStrategy(amount);
+    int price = 200;
+    int result = applyDiscount(price);
+    
+    assertEquals(100, result);
+  }
+  
+}
